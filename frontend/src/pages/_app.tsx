6caa891f64d5e4083eaa7628faf78982d3a551b4
shitakemura <shitakemura@gmail.com>
2022-06-19 11:43:39 +0900

    add amplify graphql settings

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,25 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { Amplify } from 'aws-amplify'
+
+Amplify.configure({
+  Auth: {
+    region: process.env.NEXT_PUBLIC_COGNITO_REGION,
+    userPoolId: process.env.NEXT_PUBLIC_COGNITO_USERPOOL_ID,
+    userPoolWebClientId: process.env.NEXT_PUBLIC_COGNITO_USERPOOL_WEB_CLIENT_ID,
+    mandatorySignIn: true,
+  },
+})
+
+const myAppConfig = {
+  aws_appsync_graphqlEndpoint: process.env.NEXT_PUBLIC_APPSYNC_GRAPHQL_ENDPOINT,
+  aws_appsync_region: process.env.NEXT_PUBLICK_APPSYNC_REGION,
+  aws_appsync_authenticationType: 'AMAZON_COGNITO_USER_POOLS', // You have configured Auth with Amazon Cognito User Pool ID and Web Client Id
+}
+
+Amplify.configure(myAppConfig)
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,17 +1,7 @@
 import type { NextPage } from 'next'
 
-import { Amplify } from 'aws-amplify'
 import { Authenticator } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
-
-Amplify.configure({
-  Auth: {
-    region: process.env.NEXT_PUBLIC_AWS_REGION,
-    userPoolId: process.env.NEXT_PUBLIC_COGNITO_USERPOOL_ID,
-    userPoolWebClientId: process.env.NEXT_PUBLIC_COGNITO_USERPOOL_WEB_CLIENT_ID,
-    mandatorySignIn: true,
-  },
-})
 
 const App: NextPage = () => {
   return (
